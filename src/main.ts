84edaa5ece18423b4df68e2e8e0ae6748805fb0b
DanielMedina <danielmendi17@gmail.com>
2024-07-09 17:04:59 +0200

    actualizacion

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,47 @@
 import "./style.css";
+
+let puntos = 0
+
+
+
+const muestraPuntuacion = (puntos: number) => {
+    const elementoPuntos = document.getElementById("puntuacion");
+    if (elementoPuntos && elementoPuntos instanceof HTMLOutputElement){
+        elementoPuntos.innerHTML = puntos.toString()
+    }
+}
+
+document.addEventListener("DOMContentLoaded", muestraPuntuacion);
+
+const mostrarCarta = (numeroCarta: number) : void => {
+
+        switch(numeroCarta){
+            case 1:
+                
+        }
+}
+
+function generarCarta() {
+  const numero = Math.floor(Math.random() * 10) + 1;
+  if (numero > 7) {
+    return numero + 2;
+  }
+  return numero;
+}
+
+
+
+const handleButtom () => {
+    const numeroCarta = generarCarta();
+    mostrarCarta(numeroCarta);
+
+};
+
+const botonRobar = document.getElementById("robar");
+if (botonRobar && botonRobar instanceof HTMLButtonElement) {
+  botonRobar.addEventListener("click", handleButtom);
+}
+
+
+
+console.log(numeroCarta);
